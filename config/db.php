12e77949625ab1102d66b042747ab07cdcc8f839
Herman Martinez <60677357+ciarpaglini@users.noreply.github.com>
2020-04-11 10:57:02 +0200

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 class Database{
 	public static function connect(){
-		$db = new mysqli('localhost', 'ciarpaglini', 'Austin.57', 'nopuedeser');
+		$db = new mysqli('localhost', 'xxxxxxxx', 'xxxxxxx', 'xxxxxxxx');
 		$db->query("SET NAMES 'utf8'");
 		return $db;
 	}
